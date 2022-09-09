c4d41c3ff409ae385ae9511c60861ae342602153
pramods <srinivas.pramod0@gmail.com>
2022-09-09 15:53:56 +0530

    image normalization added

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import cv2
 from keras.models import load_model
 import numpy as np
 from werkzeug.utils import secure_filename
-import os, psutil
+import os
 import gc
 from flask_cors import CORS
 import glob
+# import imageio
+# from pathlib import Path
 
 app = Flask(__name__)
 CORS(app)
@@ -17,7 +19,8 @@ def preprocess(video, start=6, end=24):
     count = 0
     img_start=0
     flag=False
-    x = np.zeros((18, 112, 112, 3))
+    X = np.zeros((18, 112, 112, 3))
+    img_list = []
     while success:
         if count==start:
             flag=True
@@ -25,24 +28,27 @@ def preprocess(video, start=6, end=24):
             flag=False
         if flag:
             image = cv2.rotate(image, cv2.ROTATE_180)
-            # image = image[360:-360,20:-20,:]
             image = cv2.resize(image, (112, 112))
-            x[img_start,:,:,:] = image
+            img_list.append(image)
+            image = (image-image.min())/(image.max()-image.min())
+            X[img_start,:,:,:] = image
             img_start = img_start+1
         success,image = vidcap.read()
         count += 1
     vidcap.release()
+    # p = ''.join(os.path.split(Path(video))[:-1])
+    # p = os.path.join(p, 'video.gif')
+    # imageio.mimsave(p, img_list, fps=10)
+    # gif = send_file(p,as_attachment=True)
     if os.path.exists(video):
         os.remove(video)
-    else:
-        print("The file does not exist")
     gc.collect()
-    return x
+    return X
 
 def prediction(file_path):
     model = load_model('models/model-00005-0.38287-0.87783-0.62432-0.85000.h5')
     x = preprocess(file_path).reshape(1, 18, 112, 112,3)
-    pred = model.predict(x)[0].argmax()
+    pred = model.predict(x)[0]
     gc.collect()
     del model
     del x
@@ -68,14 +74,15 @@ def predict():
         file_path = os.path.join(
             basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
-        process = psutil.Process(os.getpid())
-        print(process.memory_info().rss)
         gc.collect()
-        final = gestures[prediction(file_path)]
+        prob = prediction(file_path)
+        final = gestures[prob.argmax()]
+        prob = [str(i) for i in prob]
+        prob_dict = dict(zip(gestures, prob))
         del gestures
         del f
         print(final)
-        return jsonify({'prediction':final})
+        return jsonify({'prediction':final, 'probs':prob_dict})
     else:
         return "None"
 
